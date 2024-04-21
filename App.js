1b85e91d0410a01d3dd83d0ee52e9524460ead16
447241414@qq.com <447241414@qq.com>
2024-04-21 21:46:55 +0800

    feat: 独立开发者之路的准备…

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Index from './pages/index';
 import My from './pages/my';
+import Textcss from './pages/textcss';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Textcss />
       <Index />
       <My />
       <StatusBar style="auto" />
---
+++ pages/textcss/index.js
@@ -0,0 +1,63 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Textcss() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.msgBox}>
+                <Text style={styles.subTitle}>DJI Power 1000</Text>
+                <Text style={styles.title}>Power Your World</Text>
+                <Text style={styles.desc}>Buy your DJI portable power station at the official online DJI Store and enjoy up to 5 years of warranty, [6] free shipping, 1% DJI Credit back on your purchase, and more.</Text>
+                <View style={styles.btns}>
+                    <Text style={styles.buyBtn}>Buy Now</Text>
+                    <Text style={styles.moreBtn}>Watch Video</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        height: 400,
+    },
+    msgBox: {
+        //position: 'absolute',
+        //top: 20,
+        //left: 0,
+        width: '100%',
+    },
+    subTitle: {
+        color: '#000',
+        fontSize: 24,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    title: {
+        color: '#000',
+        fontSize: 18,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 12,
+    },
+    desc: {
+        color: '#000',
+        fontSize: 14,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 12,
+        paddingLeft: 12,
+        paddingRight: 12,
+        textAlign: 'center',
+    },
+    btns: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    buyBtn: {
+        flex: 1,
+    },
+    moreBtn: {
+        flex: 1,
+    }
+});
