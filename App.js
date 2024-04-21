6cb617ce0dd73745567ff9bceb538ab37185e117
447241414@qq.com <447241414@qq.com>
2024-04-21 21:23:48 +0800

    feat: 独立开发者之路的准备…

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Index from './pages/index';
-//import TextStyle from './pages/TextStyle';
+import My from './pages/my';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Index />
+      <My />
       <StatusBar style="auto" />
     </View>
   );
---
+++ pages/my/index.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function My() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text1}>index1</Text>
+            <Text>index2</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text1: {
+        color: 'red',
+        fontSize: 20,
+        padding: 20,
+    },
+});
