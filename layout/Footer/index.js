6169231fff0744596e8b73a897b7c27e2b036c45
447241414@qq.com <447241414@qq.com>
2024-05-05 17:10:25 +0800

    feat: 独立开发者之路的准备…

--- layout/Footer/index.js
+++ layout/Footer/index.js
@@ -1,17 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { TouchableWithoutFeedback, StyleSheet, Text, View } from "react-native";
 
 export default function Footer() {
+    const [list, setList] = useState([{
+        title: '11',
+    },{
+        title: '22',
+    },{
+        title: '33',
+    }]);
+    const [activeIndex, setActiveIndex] = useState(1);
     return (
         <View style={styles.container}>
-            <View style={styles.item}>
-                <View style={styles.itemTitle} onPress={() => {
-                    Alert.alert('你点击了按钮！');
-                }}><Text>11</Text></View>
-                <View style={styles.sub}>
-                    <View style={styles.subItem}><Text>22</Text></View>
-                    <View style={styles.subItem}><Text>22</Text></View>
+            {list.map((item, index) => {
+                return <View style={styles.item} key={index}>
+                    <TouchableWithoutFeedback onPress={() => {
+                        
+                        const activeIndex2 = activeIndex;
+                        setActiveIndex(activeIndex2);
+                        console.log('11', index);
+                    }}>
+                        <View style={styles.itemTitle}><Text>{item.title}:{index}</Text></View>
+                    </TouchableWithoutFeedback>
+
+                    <View style={{
+                        opacity: activeIndex === index ? 1:0
+                    }}>
+                        <View style={styles.subItem}><Text>aa</Text></View>
+                        <View style={styles.subItem}><Text>bb</Text></View>
+                    </View>
                 </View>
-            </View>
+            })}
         </View>
     );
 }
@@ -30,5 +49,8 @@ const styles = StyleSheet.create({
     },
     sub: {
         display: 'none',
+    },
+    subShow: {
+        display: 'block',
     }
 });
