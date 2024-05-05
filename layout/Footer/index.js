fa6fa203a62d4286741cc0d5a51b32c3b23af849
447241414@qq.com <447241414@qq.com>
2024-05-05 17:15:52 +0800

    feat: 独立开发者之路的准备…

--- layout/Footer/index.js
+++ layout/Footer/index.js
@@ -4,31 +4,30 @@ import { TouchableWithoutFeedback, StyleSheet, Text, View } from "react-native";
 export default function Footer() {
     const [list, setList] = useState([{
         title: '11',
-    },{
+    }, {
         title: '22',
-    },{
+    }, {
         title: '33',
     }]);
-    const [activeIndex, setActiveIndex] = useState(1);
+    const [activeIndex, setActiveIndex] = useState(null);
     return (
         <View style={styles.container}>
             {list.map((item, index) => {
                 return <View style={styles.item} key={index}>
                     <TouchableWithoutFeedback onPress={() => {
-                        
-                        const activeIndex2 = activeIndex;
-                        setActiveIndex(activeIndex2);
-                        console.log('11', index);
+                        if(activeIndex === index) {
+                            setActiveIndex(null);
+                        }else {
+                            setActiveIndex(index);
+                        }
                     }}>
-                        <View style={styles.itemTitle}><Text>{item.title}:{index}</Text></View>
+                        <View style={styles.itemTitle}><Text>{item.title}</Text></View>
                     </TouchableWithoutFeedback>
 
-                    <View style={{
-                        opacity: activeIndex === index ? 1:0
-                    }}>
+                    {activeIndex === index && <View>
                         <View style={styles.subItem}><Text>aa</Text></View>
                         <View style={styles.subItem}><Text>bb</Text></View>
-                    </View>
+                    </View>}
                 </View>
             })}
         </View>
